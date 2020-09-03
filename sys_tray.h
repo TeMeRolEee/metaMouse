08b096d85cf59812fbfc71f7ed92747fbc0f0e66
Réthalmi Dávid <david.rethalmi@gmail.com>
2020-09-03 21:13:24 +0200

    Fixed systray

--- sys_tray.h
+++ sys_tray.h
@@ -2,6 +2,7 @@
 
 #include <QSystemTrayIcon>
 #include <QDialog>
+#include <QMenu>
 #include "mouse_tracker.h"
 
 QT_BEGIN_NAMESPACE
@@ -23,48 +24,12 @@ class SysTray : public QDialog {
 	public:
 		SysTray();
 
-		void setVisible(bool visible) override;
-
-	protected:
-		void closeEvent(QCloseEvent *event) override;
-
-	private slots:
-		void setIcon(int index);
-		void iconActivated(QSystemTrayIcon::ActivationReason reason);
-		void showMessage();
-		void messageClicked();
-
 	private:
-		void createIconGroupBox();
-		void createMessageGroupBox();
-		void createActions();
 		void createTrayIcon();
 
-		QGroupBox *iconGroupBox;
-		QLabel *iconLabel;
-		QComboBox *iconComboBox;
-		QCheckBox *showIconCheckBox;
-
-		QGroupBox *messageGroupBox;
-		QLabel *typeLabel;
-		QLabel *durationLabel;
-		QLabel *durationWarningLabel;
-		QLabel *titleLabel;
-		QLabel *bodyLabel;
-		QComboBox *typeComboBox;
-		QSpinBox *durationSpinBox;
-		QLineEdit *titleEdit;
-		QTextEdit *bodyEdit;
-		QPushButton *showMessageButton;
-
-		QAction *minimizeAction;
-		QAction *maximizeAction;
-		QAction *restoreAction;
-		QAction *quitAction;
-
-		QSystemTrayIcon *trayIcon;
-		QMenu *trayIconMenu;
-
+		QSystemTrayIcon *trayIcon = nullptr;
+		QMenu *trayIconMenu = nullptr;
+		QAction *quitAction = nullptr;
 		MouseTracker *mouseTracker = nullptr;
 };
 
